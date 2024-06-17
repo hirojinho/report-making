ded004eafbdeaa6b1d577f2002b81598d5b12478
hirojinho <eduardo.haraguchi@gmail.com>
2024-06-17 01:11:06 -0300

    feat: register completion route

--- app/routes/main.py
+++ app/routes/main.py
@@ -5,3 +5,7 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/ping', methods=['GET'])
 def ping() -> Response:
     return jsonify('pong')
+
+from app.routes.completion.completion import completion
+
+main_bp.register_blueprint(completion)
