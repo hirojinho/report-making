fc8f1d6e6bbd3c72011bc205247fca1e7c9ad834
hirojinho <eduardo.haraguchi@gmail.com>
2024-07-10 00:28:58 -0300

    Refactor initiate_knowledge function to use relative path for data folder

--- app/knowledge/index.py
+++ app/knowledge/index.py
@@ -1,5 +1,9 @@
+import os
 from app.clients.embedding import EmbeddingClient
 
-def initiate_knowledge() -> EmbeddingClient:
-    knowledge = EmbeddingClient(file_path="app/knowledge/base")
+def initiate_knowledge() -> EmbeddingClient:# Get the current file's directory path
+    current_file_directory = os.path.dirname(os.path.abspath(__file__))
+    # Construct the path to the 'data' folder inside the current directory
+    data_folder_path = os.path.join(current_file_directory, 'base')
+    knowledge = EmbeddingClient(folder_path=data_folder_path)
     return knowledge
